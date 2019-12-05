23df0a03388664006508fc42dd5ac3b031cc84c4
Hima <hima.poonadathu@lcginc.com>
2019-12-05 12:06:57 -0500

    First commit

---
+++ src/sample/testpackage/Sample.java
@@ -0,0 +1,14 @@
+package sample.testpackage;
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
